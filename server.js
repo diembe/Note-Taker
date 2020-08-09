441271e3d47d333b93d5e989c0d6548bec52e4d7
diembe <contact@antibean.com>
2020-08-08 23:21:27 -0600

    changed directory structure

--- server.js
+++ server.js
@@ -13,31 +13,6 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-var characters = [
-  {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
-  }
-];
 
 // Routes
 // =============================================================
@@ -66,29 +41,10 @@ app.get("/api/notes", (req, res) => {
     });
 });
 
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
 
-  console.log(chosen);
-
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
-
-// Create New Characters - takes in JSON input
+// Create New Notes
 app.post("/api/notes", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
 	var newNote = req.body;
-
-	// Using a RegEx Pattern to remove spaces from newCharacter
-	// You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
 	newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
 
 	console.log(newNote);
